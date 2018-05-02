52884ec147cc5368b136a850df97da9d95b409c8
Brian C Sparks <brian.c.sparks@hp.com>
2018-05-01 18:21:48 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
+import {
+  Nav, Navbar, NavItem, Grid, Tabs, Tab
+}                             from 'react-bootstrap';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar inverse fluid fixedTop>
+
+          {/* Header and Brand */}
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Netlab Telemetry Viewer</a>
+
+            </Navbar.Brand>
+          </Navbar.Header>
+
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="/foobar">Foo Bar</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        {/* Main Content Area */}
+        <div>
+          <Grid fluid={true}>
+
+            <div>
+              <Tabs activeKey={1} id="top-tabs">
+                <Tab eventKey={1} title="Scratch">
+                  A tab
+                </Tab>
+              </Tabs>
+            </div>
+
+          </Grid>
+        </div>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
 
 //var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 ReactDOM.render(<App />, document.getElementById('root'));
