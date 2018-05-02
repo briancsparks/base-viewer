02b119d50c0536013a904415d38a75ee27124e03
Brian C Sparks <brian.c.sparks@hp.com>
2018-05-01 17:13:26 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
+//var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
